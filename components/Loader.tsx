5ca70782a17939b25abc0e9c6f6d82dca3c29aab
damaluku <damaluku@gmail.com>
2023-03-31 10:21:33 +0100

    Site Loader styles made global

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import styles from "../styles/Loader.module.css";
+// import styles from "../styles/Loader.module.css";
 
 const LoaderComp = () => {
   return (
     <div>
-      <div className={styles.container} id="globalLoader">
-        <span className={styles.loader}>Loading Site...</span>
+      <div className="loaderContainer" id="globalLoader">
+        <span className="loader">Loading Site &#8987;...</span>
       </div>
+      {/*  <div className={styles.container} id="globalLoader">
+        <span className={styles.loader}>Loading Site...</span>
+      </div> */}
     </div>
   );
 };
