8980fa3bd9211df86d523249945785316b9df40d
damaluku <damaluku@gmail.com>
2023-08-06 21:16:42 +0100

    link test 1

--- pages/portfolio/[projectId].tsx
+++ pages/portfolio/[projectId].tsx
@@ -164,14 +164,15 @@ const Project: React.FC<Props> = ({ data }) => {
           transition={{ duration: 0.3, delay: 1.5 }}
         >
           <Stack marginTop="4rem" direction="row" spacing={8}>
-            <Link
+            {/* <Link
               href={data?.route}
               target="_blank"
               rel="noopener"
               style={{ textDecoration: "none" }}
+              passHref
             >
               <Button variant="contained">View Live</Button>
-            </Link>
+            </Link> */}
 
             {data?.github.length > 0 && (
               <a href={data?.github} target="_blank" rel="noopener">
